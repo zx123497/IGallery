20270ef0c145ed44e264281cd1f54b4d21bcb713
leochen <leo000111444@gmail.com>
2023-02-28 01:30:57 +0800

    fix code style

--- src/components/RankingList.js
+++ src/components/RankingList.js
@@ -1,24 +1,28 @@
-import * as React from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
-import ImageIcon from '@mui/icons-material/Image';
-import WorkIcon from '@mui/icons-material/Work';
-import BeachAccessIcon from '@mui/icons-material/BeachAccess';
-import Divider from '@mui/material/Divider';
-import { Box } from '@mui/material';
+/* eslint-disable max-len */
+import * as React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import ImageIcon from "@mui/icons-material/Image";
+import WorkIcon from "@mui/icons-material/Work";
+import BeachAccessIcon from "@mui/icons-material/BeachAccess";
+import Divider from "@mui/material/Divider";
+import {Box} from "@mui/material";
+/**
+ * @return {RankingList} ranking list
+ */
 export default function RankingList() {
   return (
     <List
       sx={{
-        width: '100%',
+        width: "100%",
         maxWidth: 360,
-        bgcolor: 'background.paper',
+        bgcolor: "background.paper",
       }}
     >
-        <Box sx={{bgcolor: "#eee", padding: "1em", borderRadius: "10px 10px 0 0px"}}>Artist Ranking</Box>
+      <Box sx={{bgcolor: "#eee", padding: "1em", borderRadius: "10px 10px 0 0px"}}>Artist Ranking</Box>
       <ListItem>
         <ListItemAvatar>
           <Avatar>
