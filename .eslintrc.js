5b42bd86953bbbcf5311ca0b0bc5b2a3d87b5f69
leochen <leo000111444@gmail.com>
2023-03-05 17:39:40 +0800

    editor setting

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+  },
+  "extends": [
+    "plugin:react/recommended",
+    "google",
+  ],
+  "overrides": [
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module",
+  },
+  "plugins": [
+    "react",
+  ],
+  "rules": {
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"],
+    "max-len": ["off", {"code": 120}],
+  },
+};
