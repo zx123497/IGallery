24637a4b85a898eff7ba749c375fa1205f186c22
leochen <leo000111444@gmail.com>
2023-03-09 01:14:43 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,6 @@ module.exports = {
     "semi": ["error", "always"],
     "quotes": ["error", "double"],
     "max-len": ["off", {"code": 120}],
+    "linebreak-style": ["error", process.platform === "win32" ? "windows" : "unix"],
   },
 };
